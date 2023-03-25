7f85d5c9e149d905d5437080d7971de99b78eaf4
Cherry Ella <111384784+CherryElla@users.noreply.github.com>
2023-03-25 12:38:44 -0400

    heroku deploy fixes

--- server/server.js
+++ server/server.js
@@ -1,47 +1,52 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const path = require('path');
-const { authMiddleware } = require('./utils/auth');
-
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
+const path = require("path");
+const { authMiddleware } = require("./utils/auth");
+const http = require("http");
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const httpServer = http.createServer(app);
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if (process.env.NODE_ENV === 'production') {
-// }
+if (process.env.NODE_ENV === "production") {
+}
 
-app.use(express.static(path.join(__dirname, '../client/dist/')));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+app.use(express.static(path.join(__dirname, "../client/dist/")));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
-
 // Create a new instance of an Apollo server with the GraphQL schema
-const startApolloServer = async (typeDefs, resolvers) => {
-  await server.start();
-  server.applyMiddleware({ app });
-  
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    })
-  })
-  };
-  
+const startApolloServer = async () => {
+    console.log(`Starting server on port ${PORT}...`);
+    await server.start();
+    server.applyMiddleware({ app });
+
+    db.once("open", () => {
+        app.listen(PORT, () => {
+            console.log(`API server running on port ${PORT}!`);
+            console.log(
+                `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+            );
+        });
+    });
+};
+
 // Call the async function to start the server
-  startApolloServer(typeDefs, resolvers);
- 
+startApolloServer();
+
 // Stripe payment router
 // app.get("/", (req, res) => {
 //   const path = resolve(process.env.STATIC_DIR + "/index.html");
@@ -57,18 +62,18 @@ const startApolloServer = async (typeDefs, resolvers) => {
 // app.post("/create-payment-intent", async (req, res) => {
 
 //   try {
-    // paymentIntent is the object that contains the payment information
-  //   const paymentIntent = await stripe.paymentIntents.create({
-  //     currency: 'usd',
-  //     amount: 1099,
-  //     automatic_payment_methods: {
-  //       enable: true,
-  //     },
-  //   });
+// paymentIntent is the object that contains the payment information
+//   const paymentIntent = await stripe.paymentIntents.create({
+//     currency: 'usd',
+//     amount: 1099,
+//     automatic_payment_methods: {
+//       enable: true,
+//     },
+//   });
 
-  //   res.send({ clientSecret: paymentIntent.client_secret });
-  // } catch (e) {
-    // Return a 400 error response if the call fails
+//   res.send({ clientSecret: paymentIntent.client_secret });
+// } catch (e) {
+// Return a 400 error response if the call fails
 //     return res.status(400).send({
 //       error: {
 //         massage: e.message,
