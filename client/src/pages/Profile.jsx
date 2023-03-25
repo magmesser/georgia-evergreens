114a5c6dcf0da925a1801f1d36ed6ba0bd0a57cc
magmesser <magmesser@gmail.com>
2023-03-25 18:22:36 -0400

    when logout go to homepage instead of refresh profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -11,16 +11,21 @@ const Profile = () => {
     setCurrentForm(formName);
   };
 
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
   return (
     <>
       {Auth.loggedIn() ? (
         <div className="grid place-items-center">
-          {/* if logged in display profilesingle, if not display login/signup forms */}
+          {/* if logged in display order history, if not display login/signup forms */}
           <OrderHistory />
           <button
               className="[background-color:#a7d9d0] [color:#11402F] active:bg-neutral-700 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
               type="submit"
-              onClick={() => Auth.logout()}
+              onClick={logout}
             >
               Log Out
             </button>
