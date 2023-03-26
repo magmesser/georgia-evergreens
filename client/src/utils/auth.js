7ca0b06c5c28724954e349089296d6c2a7a28c66
magmesser <magmesser@gmail.com>
2023-03-25 20:13:53 -0400

    changing login window redirect back to how it was before

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -29,8 +29,8 @@ class AuthService {
 
   login(idToken) {
     localStorage.setItem('id_token', idToken);
-    // window.location.assign('/');
-    window.location.reload();
+    window.location.assign('/');
+    // window.location.reload();
   }
 
   logout() {
